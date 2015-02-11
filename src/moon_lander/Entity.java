3c81c6cf00fba232f43a60431bc012e69c7d9d43
Liam Dodds <liam.b.dodds@gmail.com>
2015-02-11 00:26:15 +0000

    Create Entity class

---
+++ src/moon_lander/Entity.java
@@ -0,0 +1,155 @@
+package moon_lander;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+
+import moon_lander.utility.Angle;
+import moon_lander.utility.Box2;
+import moon_lander.utility.ScreenLogger;
+import moon_lander.utility.Vector2;
+
+public class Entity {
+	/**
+	 * A random number generator used for anything an entity has need for
+	 */
+	protected Random random;
+	
+	/**
+	 * The coordinates of the entity on the map
+	 */
+	protected Point position;
+	
+	/**
+	 * The velocity of the entity in two-dimensional space
+	 */
+	protected Vector2 velocity;
+	
+	/**
+	 * The width and height of the entity
+	 */
+	protected Dimension dimensions;
+	
+	/**
+	 * The Bounding Box of the entity
+	 */
+	protected Box2 boundingBox;
+	
+	/**
+	 * The rotation angle of the entity
+	 */
+	protected Angle angle;
+	
+	/**
+	 * The speed of the entity, hurtling through space
+	 */
+	protected double acceleration;
+	
+	/**
+	 * The product to decrease the acceleration by
+	 */
+	protected double decceleration;
+	
+	/**
+	 * The entity to be drawn to the screen
+	 */
+	protected BufferedImage image;
+	
+	/**
+	 * The URL of the entity's image
+	 */
+	protected String imageURL;
+	
+	public Entity() {
+		random = new Random();
+		position = new Point(0,0);
+		velocity = new Vector2();
+		dimensions = new Dimension();
+		boundingBox = new Box2(position, dimensions);
+		angle = new Angle();
+		acceleration = 0.0;
+		decceleration = 0.5;
+	}
+	public Entity(String imageURL) {
+		this();
+		this.imageURL = imageURL;
+		this.LoadContent();
+	}
+	/**
+	 * Loading the entity content
+	 */
+	public void LoadContent() {
+		try {
+			if(imageURL != null) {
+				URL url = this.getClass().getResource(imageURL);
+				image = ImageIO.read(url);
+				dimensions.setSize(image.getWidth(), image.getHeight());
+			}
+		} catch(IOException ex) {
+			Logger.getLogger(PlayerRocket.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	/**
+	 * The entity logic method
+	 */
+	public void Update() {
+		velocity.add(
+    		(acceleration * angle.sin()), 
+    		-(acceleration * angle.cos())
+		);
+		
+		// Reduce speed
+		acceleration *= decceleration;
+		
+		position.translate((int)Math.round(velocity.getX()), (int)Math.round(velocity.getY()));
+        boundingBox.setLocation(position);
+        boundingBox.setSize(dimensions);
+	}
+	
+	/**
+	 * Drawing the entity to the screen
+	 * @param g2d
+	 */
+	public void Draw(Graphics2D g2d) {
+    	ScreenLogger.add("Entity coordinates: " + position.x + ", " + position.y);
+    	ScreenLogger.add("Entity velocity: " + velocity.getX() + ",  " + velocity.getY());
+    	ScreenLogger.add("Entity angle: " + angle.getAngle());
+    	ScreenLogger.add("Entity speed: " + acceleration);
+		
+		
+		AffineTransform old = g2d.getTransform();
+		g2d.rotate(angle.toRadians(), boundingBox.getCenterX(), boundingBox.getCenterY());
+		g2d.drawImage(image, position.x, position.y, null);
+		g2d.setTransform(old);
+	}
+	
+	public Point getPosition() {
+		return position;
+	}
+	
+	public Vector2 getVelocity() {
+		return velocity;
+	}
+	
+	public Dimension getDimensions() {
+		return dimensions;
+	}
+	
+	public Box2 getBoundingBox() {
+		return boundingBox;
+	}
+	
+	public Angle getAngle() {
+		return angle;
+	}
+}
